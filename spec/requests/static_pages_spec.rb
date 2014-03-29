1421b4e2fc505dab6439c429dea13780023976f0
Eduardo Acuña <eaculed@gmail.com>
2014-03-29 19:08:26 +0000

    Añadida pagina Contacto

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,7 @@ describe "StaticPages" do
           expect(page).to have_content('Sample App')
       end
       
-      it "should have the title 'Home'" do
+      it "should have the base title" do
           visit '/static_pages/home'
           expect(page).to have_title("Ruby on Rails Tutorial Sample App")
       end
@@ -40,6 +40,19 @@ describe "StaticPages" do
       it "should have the title 'About Us'" do
           visit '/static_pages/about'
           expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      end
+  end
+  
+  describe "Contact page" do
+      
+      it "should have the content 'Contact'" do
+          visit '/static_pages/contact'
+          expect(page).to have_content('Contact')
+      end
+      
+      it "should have the title 'Contact'" do
+          visit '/static_pages/contact'
+          expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
       end
   end
   
